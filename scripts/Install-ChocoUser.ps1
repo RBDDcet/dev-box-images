163233d5ad612917f9d933bde4ec7e6ef5553ab1
Roger Best <rbest@microsoft.com>
2023-02-24 14:16:37 -0800

    Test docker-desktop and updated install script

--- scripts/Install-ChocoUser.ps1
+++ scripts/Install-ChocoUser.ps1
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-
+[cmdletbinding()]
 Param(
     [Parameter(Mandatory=$true)]
     [string]$PackageId,
 
-    [Parameter(Mandatory=$true)]
-    [string]$PackageArgs
+    [Parameter(Mandatory=$false)]
+    [string]$PackageArguments
 )
 
 function Show-Notification {
@@ -40,13 +40,9 @@ function Show-Notification {
     $Notifier.Show($Toast);
 }
 
-
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process PowerShell -WindowStyle Hidden -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
-    exit;
-}
-
-#Check if folder exists
+# Don't use C:\Windows\Temp as requires admin elevation
+# Add logging
+# Check if folder exists
 $LogDir = "C:\Temp"
 if (-not (Test-Path -LiteralPath $LogDir)) {
     New-Item -Path $LogDir -ItemType Directory -ErrorAction Stop | Out-Null #-Force
@@ -57,25 +53,58 @@ if (-not (Test-Path -LiteralPath $Log)) {
     New-Item -Path $Log -ItemType File -ErrorAction Stop | Out-Null #-Force
 }
 
+Add-Content -Path $log -Value "Starting check to see if user has administrator privileges. $(Get-Date)"
+
+# Unable to use below as the Administrator group isn't shown.
+# ([Security.Principal.WindowsIdentity]::GetCurrent().Groups | Select-String 'S-1-5-32-544')
+
+# Check if user is in administrator group
+if ($null -ne (whoami /groups /fo csv | ConvertFrom-Csv | Where-Object { $_.SID -eq "S-1-5-32-544" })) {
+    
+    Add-Content -Path $log -Value "User has Administrator privileges: $(Get-Date)"
+  
+    # Self-elevate the script if required
+    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+        if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        
+            # Bound Arguments aren't filling in the command line.
+            #$CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.BoundArguments
+            $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" "
+        
+            foreach ($param in $MyInvocation.BoundParameters.GetEnumerator()){
+                $CommandLine += " -" + $param.key + " """ + $param.value + """"
+            }
+
+            Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine -WindowStyle Hidden
+            Exit
+        }
+    }
+}
+
+Add-Content -Path $log -Value "Deploying: $($PackageId): $($PackageArguments): $(Get-Date)"
+
 # Check if package is installed
 $toastTitle = "DevBox User Install"
 Show-Notification -ToastTitle $toastTitle -ToastText "Checking for $PackageId"
-Add-Content -Path $log -Value "Checking for ${$PackageId}: $(Get-Date)"
+Add-Content -Path $log -Value "Checking for $($PackageId): $(Get-Date)"
 $current = & choco.exe list --exact $PackageId --local-only --limit-output
 
 if (-not $current) {
     Show-Notification -ToastTitle $toastTitle -ToastText "Installing $PackageId"
-    Add-Content -Path $log -Value "Begin installing ${$PackageId}: $(Get-Date)"
-    & choco install $PackageId $PackageArgs
+    Add-Content -Path $log -Value "Begin installing $($PackageId): $(Get-Date)"
+
+    & choco.exe install $PackageId $($PackageArguments -split(" "))
+
     if ($LASTEXITCODE -ne 0) {
         Show-Notification -ToastTitle $toastTitle -ToastText "Failed Installing $PackageId"
-        Add-Content -Path $log -Value "Failed to install ${$PackageId}: $(Get-Date)"
+        Add-Content -Path $log -Value "Failed to install $($PackageId): $(Get-Date)"
+        Add-Content -Path $log -Value "Chocolatey log files are in C:\Windows\ProgramData\chocoportable\logs : $(Get-Date)"
         return 1
     }
     Show-Notification -ToastTitle $toastTitle -ToastText "Finished installing $PackageId"
-    Add-Content -Path $log -Value "Finished installing ${$PackageId}: $(Get-Date)"
+    Add-Content -Path $log -Value "Finished installing $($PackageId): $(Get-Date)"
 }
 else {
     Show-Notification -ToastTitle $toastTitle -ToastText "$PackageId already installed."
-    Add-Content -Path $log -Value "${$PackageId} already installed: $(Get-Date)"
+    Add-Content -Path $log -Value "$($PackageId) already installed: $(Get-Date)"
 }
